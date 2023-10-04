bd9617db1878b6748c63274d9124ed7ee831eea9
Antoine Stalin <19432224+stalina@users.noreply.github.com>
2023-10-04 09:32:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberBtn = document.getElementById("member-btn");
+  var skillsBtn = document.getElementById("skills-btn");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  memberBtn.style.backgroundColor = "#f7f7f7";
+  skillsBtn.style.backgroundColor = "#e9e9e9";
+} 
